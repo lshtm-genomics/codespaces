91555df5c034b2fe4e91fbd13337f7561ebed7f0
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-03-10 13:21:08 +0000

    firefox

--- .devcontainer/get-non-conda-software.sh
+++ .devcontainer/get-non-conda-software.sh
@@ -3,3 +3,8 @@ cd ~/git
 git clone https://github.com/jodyphelan/genomics_course.git
 
 bash ~/git/genomics_course/scripts/setup_machine/03_get_non_conda_programs.sh
+
+mkdir -p ~/.firefox
+echo "user_pref(\"webgl.force-enabled\", true);" > ~/.firefox/user.js
+# make alias
+echo 'alias "firefox=firefox --profile ~/.firefox/"' >> ~/.bashrc
