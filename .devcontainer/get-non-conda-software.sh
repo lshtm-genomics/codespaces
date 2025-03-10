701f76946a41550960f3bab99fc5c7e2b1773949
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-03-10 13:12:27 +0000

    udpate post-create

--- .devcontainer/get-non-conda-software.sh
+++ .devcontainer/get-non-conda-software.sh
@@ -8,4 +8,4 @@ bash ~/git/genomics_course/scripts/setup_machine/03_get_non_conda_programs.sh
 mkdir -p ~/.firefox
 echo "user_pref(\"webgl.force-enabled\", true);" > ~/.firefox/user.js
 # make alias
-echo 'alias "firefox=firefox --profile ~/.firefox/" >> ~/.bashrc'
+echo 'alias "firefox=firefox --profile ~/.firefox/"' >> ~/.bashrc
