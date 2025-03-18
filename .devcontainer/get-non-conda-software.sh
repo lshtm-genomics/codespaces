6b434fa1daf8f4fbcc638e60b94d11967c142ffc
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-03-18 09:38:50 +0700

    update

--- .devcontainer/get-non-conda-software.sh
+++ .devcontainer/get-non-conda-software.sh
@@ -3,3 +3,22 @@ cd ~/git
 git clone https://github.com/jodyphelan/genomics_course.git
 
 bash ~/git/genomics_course/scripts/setup_machine/03_get_non_conda_programs.sh
+
+
+mkdir -p ~/.firefox
+echo "user_pref(\"webgl.force-enabled\", true);" > ~/.firefox/user.js
+# make alias
+echo 'alias "firefox=firefox --profile ~/.firefox/"' >> ~/.bashrc
+
+
+ln -s ~/miniforge3/condabin/mamba ~/bin/mamba
+
+echo 'alias "ca=conda activate"' >> ~/.bashrc
+
+
+
+cd ~/git/
+git clone https://github.com/linfeng-wang/GenomicCourseGPT.git
+mamba create --name chat --file GenomicCourseGPT/linux-env.txt
+printf "cd ~/git/GenomicCourseGPT\n/home/vscode/miniforge3/envs/chat/bin/python chatbot.py\n" > ~/bin/chat
+chmod +x ~/bin/chat
