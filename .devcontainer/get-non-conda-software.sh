09a8e3e5314bb8116e8fe4f56f104c7500f636fd
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-04-27 23:23:54 +0100

    updatses

--- .devcontainer/get-non-conda-software.sh
+++ .devcontainer/get-non-conda-software.sh
@@ -3,3 +3,7 @@ cd ~/git
 git clone https://github.com/jodyphelan/genomics_course.git
 
 bash ~/git/genomics_course/scripts/setup_machine/03_get_non_conda_programs.sh
+
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda config --set channel_priority strict
